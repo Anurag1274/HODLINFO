b307d4957018fb3350491ecb658f4ce2a5089038
Anurag1274 <anuragajbhiye89@gmail.com>
2024-11-06 00:15:50 -0800

    Commit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const app = express()
 const dotenv = require('dotenv')
-const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose')
 const connectDB = require('../backend/db/db')
@@ -12,15 +11,9 @@ dotenv.config()
 const PORT = process.env.PORT || 4000
 connectDB();
 
-app.use(express.static(path.join(__dirname, '../frontend')));
-
 app.use(cors());
 app.use('/api', routes)
 
-
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
-});
 
 
 app.get('/', (req, res) => {
